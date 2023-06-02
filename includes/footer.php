e1bdd35bf36cbe0dbde5d94ad9b2d6e5dbeb5470
Ignace Van Durme <aisoftware.ds@gmail.com>
2023-06-02 18:34:44 +0800

    First Commit

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer" style="background-color: black">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+      </div>
+      <strong>Copyright &copy; 2017 Brought to You By Karlo-Ben-Jasper</strong>
+    </div>
+</footer>
